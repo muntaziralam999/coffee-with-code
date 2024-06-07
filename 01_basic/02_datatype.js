b912ecf7fc796b22708fbdd64988d6b3ee64cc56
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-07 03:12:55 +0000

    pk

---
+++ 01_basic/02_datatype.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all js code as newer version 
+// alert (3+3) wo are using nodejs not browser 
+
+// number =>2 tp power 53
+// bigint
+// string=>"" ''
+// boolean=>true/ flase
+// null =>stanalone value
+// undefind=>
+// symbol => unique
+
+
+console.log(typeof "undefined") //undefind
+console.log(typeof null) //object
+// object

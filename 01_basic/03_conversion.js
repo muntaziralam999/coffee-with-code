833eae6ecebd9f9c7849a8514e65965bd78c9cde
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-07 04:07:47 +0000

    conversion data type

---
+++ 01_basic/03_conversion.js
@@ -0,0 +1,24 @@
+let score ="33abc"
+console.log(typeof score)
+
+let valueInNumber=Number(score)
+console.log(typeof valueInNumber)
+console.log(valueInNumber)
+
+//"33"=>33
+//"33abc"=>nan
+// true=>1; flase=>0
+
+
+let isLoggedIn=1
+
+let booleanIs=Boolean(isLoggedIn)
+console.log(booleanIs);
+
+// 1=>true; 0=>false
+//""=>false
+//"hitesh"=>true
+
+let someNumber=33;
+let stringNumber=String(someNumber)
+console.log(stringNumber)

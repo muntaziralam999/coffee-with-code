1df1f9a04c92f3566fce58716ab9081b02f1a436
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-07 02:49:26 +0000

    varible_sms

---
+++ 01_basic/01_varibles.js
@@ -0,0 +1,18 @@
+const accoundId=14553
+let accoundEmail="bablu@gmil.com"
+var accoundPassword="12345"
+accountCity="jaipur"
+let accoundState;
+
+// accoundId=2  not allowed
+accoundEmail="bk@gmail.com"
+accoundPassword="23455"
+accountCity="banglore"
+
+console.table([accoundId,accoundEmail,accountCity,accoundPassword,accountCity,accoundState])
+
+/*
+prefer not to use var
+beacause of isse in block scope and functionsl scope 
+
+*/

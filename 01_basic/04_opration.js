cc7c8575304a2160ba835813713a420b9fe00ea6
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-07 05:11:27 +0000

    opt

---
+++ 01_basic/04_opration.js
@@ -0,0 +1,36 @@
+let value=3
+let negvalue=-value
+console.log(negvalue)
+
+let str1="bablu"
+let str2="hitesh"
+let str3=str1+str2
+
+
+console.log("1"+2);
+console.log(1+"2")
+console.log("1"+2+2);
+console.log(1+2+"2");
+
+console.log(+true);
+
+let num1,num2,num3
+num1=num2=num3=2+2
+
+let gamecounter=100
+gamecounter++;
+++gamecounter;
+console.log(gamecounter)
+
+// prefix and postfix
+
+let x=3
+let y=x++
+console.log(x)
+console.log(y)
+
+
+let b=3
+let c=++b
+console.log(b)
+console.log(c)

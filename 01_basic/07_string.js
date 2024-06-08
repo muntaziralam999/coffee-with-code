0480f7013aeed80968a60e8a1c73f7a168c4e552
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-08 03:23:33 +0000

    str

---
+++ 01_basic/07_string.js
@@ -0,0 +1,18 @@
+const name ="bablu"
+const repcount=50
+
+console.log(name + repcount + "value")
+
+console.log(`hello my name is ${name} and my repo is ${repcount}`)
+
+const gameName=new String("bablu_pk"); // string ko likhne ka dusra tarika 
+
+console.log(gameName[0]);
+
+console.log(gameName.charAt(2));
+console.log(gameName.includes("t"))
+
+const newstring=gameName.substring(0,4)
+console.log(newstring);
+
+console.log(gameName.split('_'));

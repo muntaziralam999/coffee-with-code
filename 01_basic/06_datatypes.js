3c1b8d6c239a7b9c9d989a6ae1f4254928704dbd
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-08 01:49:35 +0000

    ds

--- 01_basic/06_datatypes.js
+++ 01_basic/06_datatypes.js
@@ -5,3 +5,18 @@ const id=Symbol('123')
 const anotherid=Symbol('123')
 
 console.log(id ===anotherid)
+
+const obj={
+    name:"bablu",
+    class:2
+}
+
+const array=["mk","bk",123]
+
+const myFunc=function(){
+    console.log("dk")
+}
+
+console.log(typeof obj);
+console.log(typeof array);
+console.log(typeof myFunc);

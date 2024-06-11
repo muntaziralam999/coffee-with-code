c740eda8c20835cd9e55714ef595fd1b07d3ec32
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-11 03:34:22 +0000

    fun

---
+++ 03_basic/01_func.js
@@ -0,0 +1,36 @@
+
+
+function sayMyname(){
+    console.log("h");
+    console.log("i");
+    console.log("t");
+    console.log("s");
+    console.log("h");
+}
+
+// sayMyname()
+
+// function addTwoNumber(num1,num2){
+//     console.log(num1+num2)
+// }
+// // addTwoNumber(3,4)
+// const result=addTwoNumber(3,5);
+// console.log(result);
+
+function addTwoNumber(num1,num2){
+    // let result =num1+num2;
+    // return result;
+
+    return num1+num2;
+}
+
+const result =addTwoNumber(3,5)
+// console.log(result)
+
+function login(username ="sam"){
+    // return `${username} just logged in `
+
+    return username +"just logged in"
+
+}
+console.log(login("mk"))

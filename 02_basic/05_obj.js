dca0f9b13d4d6c2f64ecb4f3518d193a8c3467ed
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-11 02:31:50 +0000

    json

---
+++ 02_basic/05_obj.js
@@ -0,0 +1,13 @@
+const course={
+    coursename:"js hindi",
+    price:"999",
+    courseInstructor:"hitesh"
+}
+
+const {courseInstructor}=course; // jab obj ke data ko objectname ke sath .dot lag kr vlue assec kerte usko shor banane ke liye yani de
+                                // de starcture krne ke liye 
+const {courseInstructor:inst}=course;  // ye bhi sam kam karta isme name chnge bhi kr sakte hai 
+// console.log(courseInstructor);
+// console.log(inst);
+
+// json format

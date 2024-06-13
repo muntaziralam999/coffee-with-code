1a9d37c1a2eb2f74c2aaaead22d7ce4fe2446b5d
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-13 01:49:34 +0000

    sc

---
+++ 03_basic/03_scope.js
@@ -0,0 +1,33 @@
+var c=300;
+
+let a=500;
+
+if(true){
+    let a=10;
+    const b=20;
+    var c=30
+    // console.log(a);
+}
+
+// console.log(a);
+// console.log(b)
+// console.log(c)
+
+function one(){
+    const username="bable"
+
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    // console.log(website)
+
+    two()
+}
+
+// one()
+
+if(true){
+    cont username="bablu";
+    
+}

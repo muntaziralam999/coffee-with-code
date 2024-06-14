704ec6f5269ebd2ba19f01a0eb71e4bbe3187782
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-14 04:23:50 +0000

    c

---
+++ 04_cont/one.js
@@ -0,0 +1,110 @@
+//if 
+
+// const temp=41
+
+// if(temp<50){
+//     console.log("less than 50")
+// }
+// else{
+//     console.log("temp is grater than 50")
+// }
+
+// const score=200
+
+// if(score>100){
+//     const power="fly"
+//     console.log(`user power ${power}`)
+// }
+
+
+
+// const balance=1000;
+// // if(balance>500) console.log("test")
+
+// if (balance<500){
+//     console.log("less than 500")
+// }else if(balance<900){
+//     console.log("less than 900")
+// }else{
+//     console.log("less than 1200")
+// }
+
+
+// const userLoggedIn=true;
+// const debitCard=true
+// const loggedinfromGoogle=false;
+// const loggedinfromEmail=true
+
+
+// if(userLoggedIn && debitCard){
+//     console.log("allow to buys course")
+// }
+
+// if(userLoggedIn || debitCard){
+//     console.log("user loggedIN")
+// }
+
+// switch case satement 
+
+// switch (key) {
+//     case value:
+        
+//         break;
+
+//     default:
+//         break;
+// }
+
+// const month=3
+
+// switch (month) {
+//     case 1:
+//         console.log("january")
+//         break;
+//     case 2:
+//         console.log("feb")
+//         break;
+//     case 3:
+//         console.log("march")
+//         break;
+//     case 4:
+//         console.log("april")
+//         break;
+
+//     default:
+//         console.log("default case match")
+//         break;
+// }
+
+
+
+
+///++++++++++++++++++++++++ truthy and falsey +++++++++++++++++++++++++++++++++++++++++++++++++++
+
+// const userEmail="bk@gmail.com "
+
+// if(userEmail){
+//     console.log("got user email")
+// }
+// else{
+//     console.log("dont have user email")
+// }
+
+// falsy vales
+
+// false , 0, -0, bigInt  0n, "", null, undefined, NaN
+
+// truthy values
+// "0" , 'false'," " empity string, [] empity array , {} empity obj, function(){} empity func,
+
+const arr=[]
+
+if (arr.length===0){
+    console.log("Array is empity")
+}
+
+const obj={}
+
+if (Object.keys(obj).length===0){
+    console.log("obj is empity")
+}

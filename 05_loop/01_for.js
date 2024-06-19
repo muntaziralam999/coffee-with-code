bb39d8e4562fe5ac3dfc9dca961fb3f7388a9ed8
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-19 02:31:38 +0000

    cpp

---
+++ 05_loop/01_for.js
@@ -0,0 +1,63 @@
+// for loop
+
+// for (let mk = 0; mk < 10; mk++) {
+//     const element = mk;
+//     console.log(element)
+    
+// }
+
+// loop ke ander lopp
+
+// for (let i = 0; i <=10; i++) {
+//     console.log(`outer lop value ${i}`)
+//     for (let j = 0; j <=10; j++) {
+//          console.log(`innser loop value ${j}`)
+        
+        
+//     }
+    
+// }
+
+
+// let myArray=["flash","batman","superman"]
+
+// for (let i = 0; i < myArray.length; i++) {
+//     const element = myArray[i];
+//     console.log(element)
+    
+// }
+
+
+
+// break and continue
+
+// for (let index = 1; index <=20; index++) {
+//     console.log(index)
+//     if(index==5){
+//         console.log(`detected 5`)
+//         break;
+//     }
+    
+    
+// }
+
+
+// for (let index = 1; index <=20; index++) {
+    
+//     if(index==5){
+//         console.log(`detected 5`)
+//         continue;
+//     }
+//     console.log(index)
+    
+// }
+
+for (let i = 0; i < 10; i++) {
+    for (let j = 0; j < 10; j++) {
+        console.log("*")
+     
+    }
+    break;
+    
+    
+}

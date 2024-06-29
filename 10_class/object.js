0cd96acbdd67b79f638f968f4d52aa436a99d92f
muntaziralam999 <muntaziralam999@gmail.com>
2024-06-29 05:01:31 +0000

    pk

--- 10_class/object.js
+++ 10_class/object.js
@@ -6,16 +6,29 @@ console.log(mul(5));
 console.log(mul.power)
 console.log(mul.prototype)
 
+//++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+
 function createUser(username,score){
     this.username=username
     this.score=score
 }
+
+// create user ke ander ek naya function bana ke inject kr diya 
 createUser.prototype.increment=function(){
     this.score
 }
+
+// create user ke ander  ek aur  naya function bana ke inject kr diya 
+
+
+
 createUser.prototype.printme=function(){
     console.log(`score is ${this.score}`)
 }
+
+
+
 
 const chai=new createUser("chai",25)
 const tea=new createUser("tea",250)

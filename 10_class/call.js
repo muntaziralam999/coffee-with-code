bc8faf8ea0aa41ac2d47c7a9a15450e802617b5f
muntaziralam999 <muntaziralam999@gmail.com>
2024-07-01 01:56:28 +0000

    p

--- 10_class/call.js
+++ 10_class/call.js
@@ -3,7 +3,8 @@ function setUsername(username){
 }
 
 function createuser(username,email,password){
-    setUsername.call(this,username)
+    setUsername.call(this,username) // ye .call kam raha hai uper wale funtion ko memeory min hold karne ka 
+                                    // aur ye wla this uper wale data ko yaha reference de raha hai tab jaake call hoga 
     this.email=email
     this.password=password
 }
